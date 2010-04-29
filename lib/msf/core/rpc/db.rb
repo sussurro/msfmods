7182587e3af380cb09d9fb57434492456ead5953
sussurro <sussurro@17493a91-fda2-414d-ba78-4107cebda322>
2010-04-29 12:08:18 +0000

    upadated and renamed db.rb

--- lib/msf/core/rpc/db.rb
+++ lib/msf/core/rpc/db.rb
@@ -1,12 +1,12 @@
 module Msf
 module RPC
-class Report < Base
+class Db < Base
 
 	def db 
 		@framework.db.active
 	end
 
-	def list_workspaces(token)
+	def workspaces(token)
 		authenticate(token)
 		if(not db)
 			raise ::XMLRPC::FaultException.new(404, "database not loaded")
@@ -21,7 +21,7 @@ class Report < Base
 		res
 	end
 
-	def list_hosts(token,opts = {})
+	def hosts(token,opts = {})
 		authenticate(token)
 		if(not db)
 			raise ::XMLRPC::FaultException.new(404, "database not loaded")
@@ -34,6 +34,7 @@ class Report < Base
 	
 		@framework.db.hosts(workspace,only_up,host_search).each do |h|
 			host = {}
+			#host['created_at'] = h['created_at'].to_s
 			host['created_at'] = h['created_at'].to_s
 			host['address'] = h['address'].to_s
 			host['address6'] = h['address6'].to_s
@@ -52,7 +53,7 @@ class Report < Base
 		ret
 	end
 	
-	def list_services(token, opts = {})
+	def services(token, opts = {})
 		authenticate(token)
 		if(not db)
 			raise ::XMLRPC::FaultException.new(404, "database not loaded")
@@ -83,7 +84,7 @@ class Report < Base
 		ret
 	end	
 
-	def list_vulns(tokne,opts = {})
+	def vulns(token,opts = {})
 		authenticate(token)
 		if(not db)
 			raise ::XMLRPC::FaultException.new(404, "database not loaded")
@@ -110,6 +111,63 @@ class Report < Base
 		end
 		ret
 	end
+
+	def current_workspace(token)
+		authenticate(token)
+		if(not db)
+			raise ::XMLRPC::FaultException.new(404, "database not loaded")
+		end
+		{ "result" => "success", "workspace" => @framework.db.workspace.name }
+
+	end
+
+	#def find_workspace(name)
+	#def add_workspace(name)
+	#def get_host(opts)
+	#def find_or_create_host(opts)
+	#def report_host(opts)
+	#def each_host(wspace=workspace, &block)
+	#def find_or_create_service(opts)
+	#def report_service(opts)
+	#def get_service(wspace, host, proto, port)
+	#def each_service(wspace=workspace, &block)
+	#def get_client(opts)
+	#def find_or_create_client(opts)
+	#def report_client(opts)
+	#def each_vuln(wspace=workspace,&block)
+	#def each_note(wspace=workspace, &block)
+	#def find_or_create_note(opts)
+	#def report_note(opts)
+	#def notes(wspace=workspace)
+	#def report_auth_info(opts={})
+	#def get_auth_info(opts={})
+	#def find_or_create_vuln(opts)
+	#def report_vuln(opts)
+	#def get_vuln(wspace, host, service, name, data='')
+	#def find_or_create_ref(opts)
+	#def get_ref(name)
+	#def del_host(wspace, address, comm='')
+	#def del_service(wspace, address, proto, port, comm='')
+	#def has_ref?(name)
+	#def has_vuln?(name)
+	#def has_host?(wspace,addr)
+	#def events(wspace=workspace)
+	#def report_event(opts = {})
+	#def each_loot(wspace=workspace, &block)
+	#def find_or_create_loot(opts)
+	#def report_loot(opts)
+	#def loots(wspace=workspace)
+	#def import_msfe_v1_xml(data, wspace=workspace)
+	#def import_nexpose_simplexml(data, wspace=workspace)
+	#def import_nexpose_rawxml(data, wspace=workspace)
+	#def import_nmap_xml(data, wspace=workspace)
+	#def import_nessus_nbe(data, wspace=workspace)
+	#def import_openvas_xml(filename)
+	#def import_nessus_xml(data, wspace=workspace)
+	#def import_nessus_xml_v2(data, wspace=workspace)
+	#def import_qualys_xml(data, wspace=workspace)
+	#def import_ip_list(data, wspace)
+	#def import_amap_mlog(data, wspace)
 end
 end
 end
